354ad5a31a80f4c9dbbab29ed286292281232d03
0x9dhcf <0x9dhcf@gmail.com>
2020-05-01 11:25:00 -0700

    rename include guard.

--- x11.h
+++ x11.h
@@ -20,8 +20,8 @@
  * SOFTWARE.
  */
 
-#ifndef __MWM_X11_H__
-#define __MWM_X11_H__
+#ifndef __X11_H__
+#define __X11_H__
 
 #include <xcb/xcb.h>
 #include <xcb/xcb_ewmh.h>
@@ -29,7 +29,6 @@
 #include <xkbcommon/xkbcommon.h>
 #include <xkbcommon/xkbcommon-compose.h>
 #include <xkbcommon/xkbcommon-x11.h>
-
 
 /* atoms */
 enum {
@@ -41,17 +40,16 @@ enum {
     MWM_ATOM_COUNT
 };
 
-extern xcb_connection_t       *g_xcb;
-extern int                     g_screen_id;
-extern xcb_screen_t           *g_screen;
-extern xcb_window_t            g_root;
-extern xcb_visualtype_t       *g_visual;
-extern xcb_ewmh_connection_t   g_ewmh;
-extern xcb_atom_t              g_atoms[MWM_ATOM_COUNT];
-extern struct xkb_state       *g_xkb_state;
+extern xcb_connection_t         *g_xcb;
+extern int                      g_screen_id;
+extern xcb_screen_t             *g_screen;
+extern xcb_window_t             g_root;
+extern xcb_visualtype_t         *g_visual;
+extern xcb_ewmh_connection_t    g_ewmh;
+extern xcb_atom_t               g_atoms[MWM_ATOM_COUNT];
+extern struct xkb_state         *g_xkb_state;
 
 void x11_setup();
 void x11_cleanup();
 
 #endif
-
